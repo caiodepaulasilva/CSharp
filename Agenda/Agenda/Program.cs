1f6e61ed2c1d81257c5b2fd1549b50858f802e54
caiodepaulasilva <caiodepaula.silva@gmail.com>
2019-12-09 12:38:41 -0300

    Subir atividade

---
+++ Agenda/Agenda/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contatos contatos = new Contatos();
+            contatos.Controle();
+        }
+    }
+
+    public class Contatos
+    {
+
+        List<Usuário> usuarios = new List<Usuário>();
+        Usuário usuário = new Usuário();
+
+        public struct Usuário
+        {
+            public string name { get; set; }
+            public string number { get; set; }
+            public string email { get; set; }
+            public string group { get; set; }
+
+            public bool Contains(string filtro)
+            {
+                if (name == filtro || number == filtro || email == filtro || group == filtro) { return true; }
+                else { return false; }
+            }
+
+            public void NovoContato()
+            { Console.WriteLine($"Nome: {name} | Tel: {number} | E-mail: {email} | Grupo: {group}"); }
+        }
+
+        public void ApresentarMenu()
+        {
+            Console.WriteLine("=============== AGENDA ==============");
+            Console.WriteLine("\nMENU\n");
+            Console.WriteLine("1 - Buscar um contato");
+            Console.WriteLine("2 - Adicionar um contato");
+            Console.WriteLine("3 - Editar um contato");
+            Console.WriteLine("4 - Remover um contato");
+            Console.WriteLine("5 - Sair");
+        }
+
+        public void Controle()
+        {
+            while (true)
+            {
+                Console.Clear();
+                ApresentarMenu();
+
+                Console.Write("\nDigite: ");
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Buscar();
+                        break;
+                    case 2:
+                        Adicionar();
+                        break;
+                    case 3:
+                        Editar();
+                        break;
+                    case 4:
+
+                        Remover();
+                        break;
+                    case 5:
+                        Sair();
+                        break;
+
+                    default:
+                        Console.WriteLine("Serviço Inválido");
+                        break;
+                }
+            }
+        }
+
+        public void Buscar()
+        {
+            Console.WriteLine("\n======================================");
+            Console.Write("                Buscar");
+            Console.WriteLine("\n======================================");
+            Console.Write("Buscar: ");
+
+            string filtro = Console.ReadLine();
+            Console.WriteLine("");
+
+            try
+            {
+                usuário = usuarios[usuarios.FindIndex(usuário => usuário.Contains(filtro))];
+                Console.WriteLine($"Nome: {usuário.name}\n" +
+                                  $"Telefone: {usuário.number}\n" +
+                                  $"E-mail: {usuário.email}\n" +
+                                  $"Grupo: {usuário.group}");
+            }
+            catch { Console.WriteLine("nenhum contato foi encontrado"); }
+
+            Console.WriteLine("======================================\n\n\n");
+            Console.Write("Pressione qualquer tecla para continuar");
+            Console.ReadLine();
+        }
+
+        private void Adicionar()
+        {
+            Console.WriteLine("\n======================================");
+            Console.Write("          Novo Registro");
+            Console.WriteLine("\n======================================");
+            Console.Write("Nome: ");
+
+            usuário.name = Console.ReadLine();
+
+            Console.Write("Número: ");
+            usuário.number = Console.ReadLine();
+
+            Console.Write("E-mail: ");
+            usuário.email = Console.ReadLine();
+
+            Console.Write("Grupo: ");
+            usuário.group = Console.ReadLine();
+
+            Console.WriteLine("======================================\n\n\n");
+            Console.Write("Pressione qualquer tecla para continuar");
+            Console.ReadLine();
+
+            usuarios.Add(usuário);
+        }
+
+        private void Remover()
+        {
+            Console.WriteLine("\n======================================");
+            Console.Write("                Remover");
+            Console.WriteLine("\n======================================");
+            Console.Write("Remover: ");
+
+            string filtro = Console.ReadLine();
+
+            try
+            {
+                usuário = usuarios[usuarios.FindIndex(usuário => usuário.Contains(filtro))];
+                Console.WriteLine($"nome: {usuário.name} | tel: {usuário.number} | e-mail: {usuário.email} | grupo: {usuário.group}");
+                usuarios.Remove(usuário);
+                Console.WriteLine("O contato foi removido!");
+            }
+            catch { Console.WriteLine("nenhum contato foi encontrado"); }
+
+            Console.WriteLine("======================================\n\n\n");
+            Console.Write("Pressione qualquer tecla para continuar");
+            Console.ReadLine();
+        }
+
+        private void Editar() { Console.WriteLine("Editar Contatos"); }
+
+        private int Sair() { return 0; }
+    }
+}
---
+++ TranscreverNumeros/TranscreverNúmeros/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranscreverNúmeros
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Tradutor tradutor = new Tradutor();
+            tradutor.EntrarNumeros();
+            tradutor.OrdenarNumeros();
+            tradutor.TranscreverNumeros();
+        }
+    }
+
+    class Tradutor
+    {
+
+        int[] numbers;
+
+        Dictionary<int, string> unidades = new Dictionary<int, string>() { { 0, "zero" }, { 1, "one" }, { 2, "two" }, { 3, "trhee" }, { 4, "four" }, { 5, "five" }, { 6, "six" }, { 7, "seven" }, { 8, "eight" }, { 9, "nine" }, { 10, "ten" }, { 11, "eleven" }, { 12, "twelve" }, { 13, "thirteen" }, { 14, "fourteen" }, { 15, "fiveteen" }, { 16, "sixteen" }, { 17, "seventeen" }, { 18, "eighteen" }, { 19, "nineteen" }};
+        Dictionary<int, string> x10 = new Dictionary<int, string>() { { 1, "ten" }, { 2, "twenty" }, { 3, "thirty" }, { 4, "forty" }, { 5, "fifty" }, { 6, "sixty" }, { 7, "seventy" }, { 8, "eighty" }, { 9, "ninety" } };
+        Dictionary<int, string> x100 = new Dictionary<int, string>() { { 1, "hundred" } };
+
+        public void EntrarNumeros()
+        {
+            Console.Write($"1 - Define a length of numbers to verify: ");
+            int length = int.Parse(Console.ReadLine());
+            numbers = new int[length];
+            Console.WriteLine("\nSet:");
+
+            for (var number = 0; number < length; number++)
+            {
+                Console.Write("-");
+                numbers.SetValue(int.Parse(Console.ReadLine()), number);
+            }
+        }
+
+        public void OrdenarNumeros()
+        {
+            Array.Sort(numbers);
+            Console.WriteLine("\n2 - The numbers will be arranged in ascending order");
+        }
+
+        public void TranscreverNumeros()
+        {
+            var value = "";
+
+            void getDec(int number)
+            {
+              
+              
+                if (number < 20)
+                { Console.WriteLine(unidades.TryGetValue(number, out value) ? value : "non defined"); }
+
+                else
+                {
+                    if (number % 10 == 0)
+                    { Console.WriteLine(x10.TryGetValue(number, out value) ? value : "non defined"); }
+                    else
+                    {
+                         
+                        Console.Write(x10.TryGetValue(int.Parse(number.ToString().Substring(0, 1)), out value) ? value  + " ": "non defined");
+                        Console.WriteLine(unidades.TryGetValue(int.Parse(number.ToString().Substring(1, 1)),out value) ? value : "non defined" );
+                    }
+                }
+            }
+            void getCent(int number)
+
+                
+            {
+                if (number % 100 == 0)
+                {
+                    Console.Write(unidades.TryGetValue(int.Parse(number.ToString().Substring(0,1)), out value) ? value  + " ": "non defined");
+                    Console.Write(x100.TryGetValue(1, out value) ? value  + " " : "non defined");                   
+                }
+                else
+                {
+                    Console.Write(unidades.TryGetValue(int.Parse(number.ToString().Substring(0, 1)), out value) ? value  + " " : "non defined");
+                    Console.Write(x100.TryGetValue(1, out value) ? value + " and ": "non defined" );             
+                    getDec(int.Parse(number.ToString().Substring(1, 2)));
+                }
+            }
+
+            Console.WriteLine("\n3 - The organized and transcribed numbers:");
+            Console.WriteLine("Get:");
+
+            foreach (var number in numbers)
+            {
+                if (number < 100)
+                {
+                    getDec(number);
+                }
+                else if (number >= 100 && number < 1000)
+                {
+                    getCent(number);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
